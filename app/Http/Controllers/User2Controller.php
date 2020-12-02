0151c4b3acbae82aa509fb25f994878aad91345e
dibdibnathalie <nathaliejonicole15@gmail.com>
2020-12-03 00:56:45 +0800

    12/03

---
+++ app/Http/Controllers/User2Controller.php
@@ -0,0 +1,63 @@
+<?php
+
+    namespace App\Http\Controllers;
+    
+    use Illuminate\Http\Response;
+    use Illuminate\Http\Request;
+    use App\Traits\ApiResponser;
+    use App\Models\User;
+    use App\Services\User2Service;
+    use DB;
+
+    Class User2Controller extends Controller 
+    {
+        use ApiResponser;
+
+        /**
+         * The service to consume the User2 Microservice
+         * @var User2Service
+         */
+
+        public $user2Service;
+        /**
+         * Create a new controller instance
+         * @return void
+         */
+
+        public function __construct(User2Service $user2Service)
+        {
+            $this->user2Service = $user2Service;
+        }
+
+
+        public function index()
+        {
+            //
+            return $this->successResponse($this->user2Service->obtainUsers2());
+            
+        }
+        
+        //add user function
+        public function add(Request $request)
+        {
+            return $this->successResponse($this->user2Service->create2User($request->all(), Response::HTTP_CREATED));
+        }
+
+        //specific user search function
+        public function show($id)
+        {
+            return $this->successResponse($this->user2Service->obtainUser2($id));
+        }
+           
+
+        public function update(Request $request, $id)
+        {   
+            return $this->successResponse($this->user2Service->editUser2($request->all(),$id));
+        }
+
+
+        public function delete($id)
+        {
+            return $this->successResponse($this->user2Service->deleteUser2($id));
+        }
+    }
